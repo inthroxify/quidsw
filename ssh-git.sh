90038c883cd8779422c70d75a8f223ace5b91a56
inthroxify <inthroxify@users.noreply.github.com>
2016-11-04 08:42:39 -0600

    initial import

---
+++ ssh-git.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CMDARGS=""
+if ! [ -z "$PKEY" ]; then
+	CMDARGS="${CMDARGS} -i \"$PKEY\""
+fi
+
+if ! [ -z "$SSHCONF" ]; then
+	CMDARGS="${CMDARGS} -F \"$SSHCONF\""
+fi
+
+ssh $CMDARGS "$@"
